50dd0115c7b00b8cde15cd8e4a73d498b931bcfa
anna <a1b2v3@mail.ru>
2022-12-07 23:12:45 +0500

    add Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (n>=100)
+{ 
+n = n/100;
+n = n%10;
+
+Console.WriteLine(n);
+}
+else
+{
+Console.WriteLine("Третьей цифры нет");   
+}
