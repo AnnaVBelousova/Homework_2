41e1b5872328339a81b484e7453ddbfea4d5710a
anna <a1b2v3@mail.ru>
2022-12-07 23:08:37 +0500

    add Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int n = Convert.ToInt32(Console.ReadLine());
+n = n/10;
+n = n%10;
+Console.WriteLine(n);
